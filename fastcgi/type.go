482a7b33b3a30742bc6076bb2fe4ec9c4076e2d2
Bogdante <bogdantegiero@gmail.com>
2025-10-01 01:16:25 +0300

    added fastcgi types

---
+++ fastcgi/type.go
@@ -0,0 +1,42 @@
+package fastcgi
+
+type RequestContext struct {
+	RequestId uint16
+	Params    map[string]string
+	StdIn     []byte
+	StdOut    []byte
+}
+
+type RecordType uint8
+
+const (
+	BeginRequest RecordType = iota + 1
+	AbortRequest
+	EndRequest
+	Params
+	StdIn
+	StdOut
+	StdErr
+	Data
+)
+
+type Header struct {
+	version       uint8
+	Type          RecordType
+	RequestId     uint16
+	ContentLength uint16
+	PaddingLength uint8
+	_             uint8
+}
+
+type BeginRequestBody struct {
+	Role  uint16
+	Flags byte
+	_     [5]byte
+}
+
+type EndRequestBody struct {
+	AppStatus      uint32
+	ProtocolStatus uint8
+	_              [3]byte
+}
